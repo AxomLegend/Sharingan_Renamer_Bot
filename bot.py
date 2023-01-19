41b50bf0c32f81de48b11e3479c65bfb9d7b1fe7
Dlaize Kohli <dakshkohli23@gmail.com>
2023-01-19 14:47:04 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ class Bot(Client):
 
     def __init__(self):
         super().__init__(
-            name="RenameBot",
+            name="Rename Bot",
             api_id=API_ID,
             api_hash=API_HASH,
             bot_token=TG_BOT_TOKEN,
