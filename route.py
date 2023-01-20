5fbef47293d87f50bbd6999f8bd3ec694bad3f6e
Dlaize Kohli <dakshkohli23@gmail.com>
2023-01-20 11:48:24 +0530

    Update route.py

--- route.py
+++ route.py
@@ -4,7 +4,7 @@ routes = web.RouteTableDef()
 
 @routes.get("/", allow_head=True)
 async def root_route_handler(request):
-    return web.json_response("Sharingan2023")
+    return web.json_response("PyroBotz")
 
 
 async def web_server():
