9f19b8653cf39b07a5fd759bd761d7a681d8782a
Dlaize Kohli <dakshkohli23@gmail.com>
2023-02-06 10:50:06 +0530

    Update cb_data.py

--- plugins/cb_data.py
+++ plugins/cb_data.py
@@ -3,7 +3,7 @@ from pyrogram import Client, filters
 from pyrogram.types import (  InlineKeyboardButton, InlineKeyboardMarkup,ForceReply)
 from hachoir.metadata import extractMetadata
 from hachoir.parser import createParser
-from helper_funcs.database import db
+from helper_funcs.database import find
 import os 
 import humanize
 from PIL import Image
